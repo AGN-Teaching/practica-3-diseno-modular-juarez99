9af2cb98d10b0f92c35570397ac630894ece0751
Abel García Nájera <abel.garcia.n@gmail.com>
2025-03-31 14:20:34 -0600

    Initial commit

---
+++ analisis_texto.py
@@ -0,0 +1,24 @@
+"""
+Incluye funciones para contar el número de sílabas,
+de palabras y de oraciones contenidas en un texto
+que se recibe como entrada.
+"""
+
+
+def contar_oraciones(texto):
+    """Cuenta y devuelve el número de oraciones en texto."""
+    
+    # Completar
+    
+
+def contar_palabras(texto):
+    """Cuenta y devuelve el número de palabras en texto."""
+    
+    # Completar
+
+
+def contar_silabas(texto):
+    """Cuenta y devuelve el número de sílabas en texto."""
+    
+    # Completar
+
---
+++ legibilidad.py
@@ -0,0 +1,17 @@
+"""
+Funciones para calcular el índice de Fernandez Huerta (utilizando el módulo analisis_texto) y para decidir el nivel de legibilidad de un texto.
+"""
+
+import analisis_texto
+
+
+def indice_FH(texto):
+    """Calcula y devuelve el índice de Fernández Huerta del texto de entrada."""
+    
+    # Completar
+
+
+def nivel_legibilidad_FH(indice):
+    """Decide y devuelve el nivel de legibilidad de un texto de acuerdo con el índice de Fernández Huerta."""
+
+    # Completar
