0506fdad1ed7f4b43f23d7b9f8aeff15cc3de5e2
juarez99 <125591350+juarez99@users.noreply.github.com>
2025-04-11 13:55:16 -0600

    Update analisis_texto.py

--- analisis_texto.py
+++ analisis_texto.py
@@ -1,54 +1,68 @@
-"""
-Funciones para calcular el índice de Fernandez Huerta (utilizando el módulo
-analisis
+def contar
 _
-texto) y para decidir el nivel de legibilidad de un texto.
-"""
-import analisis
+oraciones(texto):
+# Se crea una lista de signos que marcan el fin de una oración.
+signos
 _
-texto as at # se llava a los módulos de el script
-'analisis
+puntuacion = ['
+.
+'
+,
+'?'
+,
+'!'
+,
+';'
+,
+':']
+# Crear un contador de oraciones.
+contador
 _
-texto' para poder crear el índice
-def indice
+oraciones = 0
+# el for revisara cada carácter del texto.
+for caracter in texto:
+if caracter in signos
 _
-FH(texto):
-"""Calcula y devuelve el índice de Fernández Huerta del texto de
-entrada.
-"""
-oraciones = at.contar
+puntuacion:
+# cuando detecte un signo de puntiación se iran contando las
+oraciones
+contador
 _
-oraciones(texto)
-palabras = at.contar
-_palabras(texto)
-silabas = at.contar
+oraciones += 1
+return contador
 _
-silabas(texto)
-"""L = 206.84 - 1.02 (palabras / oraciones) - 60 (sílabas / palabras)"""
-indice = 206.84 - 1.02 * (palabras / oraciones) - 60 * (silabas /
-palabras)
-return oraciones, palabras, silabas, indice
-def nivel
+oraciones
+def contar
 _
-legibilidad
+palabras(texto):
+# Dividir el texto en palabras utilizando split()
+palabras = texto.split()
+return len(palabras)
+def contar
 _
-FH(indice):
-"""Decide y devuelve el nivel de legibilidad de un texto de acuerdo con
-el índice de Fernández Huerta.
-"""
-if indice >= 90: # se utiliza una estructura de control selectiva para
-evaluar el nivel de legibilidad
-nivel = 'Muy fácil'
-elif indice >= 80:
-nivel = 'Fácil'
-elif indice >= 70:
-nivel = 'Relativamente fácil'
-elif indice >= 60:
-nivel = 'Normal'
-elif indice >= 50:
-nivel = 'Relativamente difícil'
-elif indice >= 30:
-nivel = 'Difícil'
-else:
-nivel = 'Muy difícil'
-return nivel
+silabas(texto):
+texto = texto.lower()
+vocales = "aeiouáéíóú"
+cantidad
+_
+silabas = 0
+if texto[-1] in '
+.,;:!?':
+texto = texto[:-1]
+if texto[-1] == 's':
+texto = texto[:-1]
+if texto[-2:] == 'es':
+texto = texto[:-2]
+# se dividen las palabras y se revisa una por una hasta encontrar una vocal e
+irlas almacenando en el contador de silabas
+for palabra in texto.split():
+if palabra[0] in vocales:
+cantidad
+_
+silabas += 1
+for i in range(1, len(palabra)):
+if palabra[i] in vocales and palabra[i - 1] not in vocales:
+cantidad
+_
+silabas += 1
+return cantidad
