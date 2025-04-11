e7d221dbd2511ee827351351159b3bcb49503fd6
juarez99 <125591350+juarez99@users.noreply.github.com>
2025-04-11 13:54:17 -0600

    Update analisis_texto.py

--- analisis_texto.py
+++ analisis_texto.py
@@ -1,24 +1,54 @@
 """
-Incluye funciones para contar el número de sílabas,
-de palabras y de oraciones contenidas en un texto
-que se recibe como entrada.
+Funciones para calcular el índice de Fernandez Huerta (utilizando el módulo
+analisis
+_
+texto) y para decidir el nivel de legibilidad de un texto.
 """
-
-
-def contar_oraciones(texto):
-    """Cuenta y devuelve el número de oraciones en texto."""
-    
-    # Completar
-    
-
-def contar_palabras(texto):
-    """Cuenta y devuelve el número de palabras en texto."""
-    
-    # Completar
-
-
-def contar_silabas(texto):
-    """Cuenta y devuelve el número de sílabas en texto."""
-    
-    # Completar
-
+import analisis
+_
+texto as at # se llava a los módulos de el script
+'analisis
+_
+texto' para poder crear el índice
+def indice
+_
+FH(texto):
+"""Calcula y devuelve el índice de Fernández Huerta del texto de
+entrada.
+"""
+oraciones = at.contar
+_
+oraciones(texto)
+palabras = at.contar
+_palabras(texto)
+silabas = at.contar
+_
+silabas(texto)
+"""L = 206.84 - 1.02 (palabras / oraciones) - 60 (sílabas / palabras)"""
+indice = 206.84 - 1.02 * (palabras / oraciones) - 60 * (silabas /
+palabras)
+return oraciones, palabras, silabas, indice
+def nivel
+_
+legibilidad
+_
+FH(indice):
+"""Decide y devuelve el nivel de legibilidad de un texto de acuerdo con
+el índice de Fernández Huerta.
+"""
+if indice >= 90: # se utiliza una estructura de control selectiva para
+evaluar el nivel de legibilidad
+nivel = 'Muy fácil'
+elif indice >= 80:
+nivel = 'Fácil'
+elif indice >= 70:
+nivel = 'Relativamente fácil'
+elif indice >= 60:
+nivel = 'Normal'
+elif indice >= 50:
+nivel = 'Relativamente difícil'
+elif indice >= 30:
+nivel = 'Difícil'
+else:
+nivel = 'Muy difícil'
+return nivel
